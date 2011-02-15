99c17dcb19dd4caa2039f18910e8f5ba654ee9a2
thechitowncubs <thechitowncubs@gmail.com>
2011-02-15 01:36:05 -0600

    help

--- .bashrc
+++ .bashrc
@@ -1,8 +1,1 @@
-
-# Check for an interactive session
-[ -z "$PS1" ] && return
-
-alias ls='ls --color=auto'
-PS1='[\u@\h \W]\$ '
-
-export BROWSER="chromium"
+/home/john/.bashrc
--- .config/wmfs/wmfs-bars.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-while [ "$(pidof wmfs)" ]
-do
-  conky | while read -r; do wmfs -s -name "\\#8BD640\\$REPLY \\#D3D7CF\\$(date +%r,\ %A\ %x)"; done
-  sleep 1 
-done
