1fd1532dbd4bc91750100f22776bbee8044f1a15
thechitowncubs <thechitowncubs@gmail.com>
2011-02-16 00:00:32 -0600

    fixed status.sh

--- .config/wmfs/status.sh
+++ .config/wmfs/status.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-do
-  conky | while read -r; do wmfs -s -name "$REPLY \\#FFFFFF\| \\#D4D4D4\\$(date +%r,\ %A\ %x)"; done
-done
+conky | while read -r; do wmfs -s -name "$REPLY \\#FFFFFF\| \\#D4D4D4\\$(date +%r,\ %A\ %x)"; done
